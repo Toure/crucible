b123eb7882105c5e7b23500dbc85b0a2baccd081
Toure Dunnon <toure@redhat.com>
2014-08-20 16:20:26 -0400

    completed firewall setup and most of libvirtd setup.

--- task/setup.py
+++ task/setup.py
@@ -25,6 +25,7 @@ class Base(OSVer):
         self.nova_config = None
         self.share_storage_config = None
         self.system_info_config = None
+        self.nova_hosts = None
 
     def make_config_obj(self, cfgname, path):
         """
@@ -58,8 +59,9 @@ class Base(OSVer):
                 bucket[value] = None
         return value
 
-    def rmt_copy(self, hostname, get=False, send=False, fname=None):
+    def rmt_copy(self, hostname, get=False, send=False, fname=None, remote_path=None):
         """Remote copy function retrieves files from specified host.
+        :param remote_path: where to place the file on the other end.
         :param hostname: host name or ip address
         :param get: flag to receive files
         :param send: flag to send files
@@ -70,9 +72,10 @@ class Base(OSVer):
         ssh.connect(hostname)
         scp = SCPClient(ssh.get_transport())
         if get:
+            os.chdir('/tmp')
             scp.get(fname)
         elif send:
-            scp.put(fname)
+            scp.put(fname, remote_path=remote_path)
 
     def rmt_exec(self, hostname, cmd):
         ssh = SSHClient()
@@ -80,13 +83,22 @@ class Base(OSVer):
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
         return ssh_stdin, ssh_stdout, ssh_stderr
 
-    def adj_val(self, token, value, filename=None):
+    def adj_val(self, token, value, oldfile=None, newfile=None):
         """Change the value of the token in a given config file.
         :param token: key within the config file
         :param value: value for token
         :param filename: specified config file
         """
-        pass
+        r = open(oldfile, 'r')
+        w = open(newfile, 'w')
+        lines = r.readlines()
+        for line in lines:
+            line.split('=')
+            if token in line[0]:
+                if line[0].startswith('#'):
+                    line[0] = line[0].replace('#', '')
+                line[1] = value
+            w.write('='.join(line))
 
     def system_setup(self):
         """
@@ -94,41 +106,67 @@ class Base(OSVer):
         """
         #rhel_ver = {6: "upstart", 7: "systemd"}
         #rhel_env = rhel_ver[OSVer.system_version()]
-        self.system_info_config = self.make_config('sys_info', '../configs/system_info')
+        self.system_info_config = self.make_config_obj('sys_info', '../configs/system_info')
         answerfile = self.config_gettr(self.system_info_config, 'packstack')['filename']
         run_setup.generateAnswerFile(answerfile)
 
         if os.path.exists(answerfile):
-            nova_compute_hosts = self.config_gettr(self.system_info_config, 'nova')['NOVA_COMPUTE_HOSTS']
+            self.nova_hosts = self.config_gettr(self.system_info_config, 'nova')['NOVA_COMPUTE_HOSTS']
             answer_file = self.make_config_obj('packstack_ans', answerfile)
             try:
-                answer_file.set('general', 'CONFIG_COMPUTE_HOSTS', nova_compute_hosts)
+                answer_file.set('general', 'CONFIG_COMPUTE_HOSTS', self.nova_hosts)
             except IOError as e:
                 print e.message
             run_setup._main(configFile=answerfile)
-
         else:
             print("Couldn't find packstack answer file")
             exit()
 
-    def firewall_setup(self, hostname):
-        """
-
-        :param hostname:
-        """
+    def firewall_setup(self):
         self.firewall_config = self.make_config_obj('firewall_rules', '../configs/firewall')
-        nfs_tcp = self.config_gettr(self.firewall_config, 'nfs rules')[tcp_ports]
-        nfs_udp = self.config_gettr(self.firewall_config, 'nfs rules')[udp_ports]
-        libvirtd_tcp = self.config_gettr(self.firewall_config, 'libvirtd rules')[tcp_ports]
+        nfs_tcp = self.config_gettr(self.firewall_config, 'nfs rules')['tcp_ports']
+        nfs_udp = self.config_gettr(self.firewall_config, 'nfs rules')['udp_ports']
+        libvirtd_tcp = self.config_gettr(self.firewall_config, 'libvirtd rules')['tcp_ports']
 
-        cmd = 'iptables -A INPUT -p tcp --dport %s -j ACCEPT' % ports
-
-        for host in hostname:
+        for host in self.nova_hosts:
             for ports in [nfs_tcp, nfs_udp, libvirtd_tcp]:
+                cmd = u"iptables -A INPUT -p tcp --dport {0:s} -j ACCEPT".format(ports)
                 self.rmt_exec(host, cmd)
 
     def libvirtd_setup(self):
-        pass
+        self.libvirtd_config = self.make_config_obj('libvirt_conf', '../configs/libvirtd')
+        _libvirtd_conf = dict(self.libvirtd_config.items('libvirtd_conf'))
+        _libvirtd_sysconf = dict(self.libvirtd_config.items('libvirtd_sysconfig'))
+
+        for _dict_obj in [_libvirtd_conf, _libvirtd_sysconf]:
+            self.rmt_copy(self.nova_hosts[0], get=True, fname=_dict_obj['filename'])
+
+        for name, value in _libvirtd_conf:
+            self.adj_val(name, value, oldfile='libvirtd.conf', newfile='libvirtd.conf.new')
+        self.adj_val('LIBVIRTD_ARGS', 'listen', oldfile='libvirtd', newfile='libvirtd.new')
+
+        try:
+            os.rename('libvirtd.conf', 'libvirtd.conf.org')
+            os.rename('libvirtd', 'libvirtd.org')
+        except IOError as ie:
+            print ie.message
+            exit()
+
+        try:
+            os.rename('libvirtd.conf.new', 'libvirtd.conf')
+            os.rename('libvirtd.new', 'libvirtd')
+        except IOError as ie:
+            print ie.message
+            exit()
+
+        for host in self.nova_hosts:
+            for _obj in [_libvirtd_conf, _libvirtd_sysconf]:
+                file = _obj['filename']
+                file = file.split('/')
+                file_path = ('/'.join(file[1:3]))
+                self.rmt_copy(host, send=True, fname=file[3], remote_path=file_path)
+
+
 
     def nova_setup(self):
         pass
